93e85b06f1490c6ab9d6ff8e59833c944be2b7c5
Alex Precosky <precosky@mac.com>
2024-06-15 20:54:56 -0700

    Adds graphics for and draws the main level background tilemap

--- src/level.c
+++ src/level.c
@@ -1,7 +1,19 @@
 #include "level.h"
+#include "res/level_map.h"
+#include "res/level_map_tiles.h"
 #include <gb/gb.h>
 #include <gbdk/console.h>
 #include <stdio.h>
+
+void init_gfx(void)
+{
+    // Load Background tiles and then map
+    set_bkg_data(0, 18u, level_map_tiles);
+    set_bkg_tiles(0, 0, level_mapWidth, level_mapHeight, level_map);
+
+    // Turn the background map on to make it visible
+    SHOW_BKG;
+}
 
 void run_level()
 {
@@ -9,6 +21,8 @@ void run_level()
 
     gotoxy(0, 8);
     printf("### LEVEL START ###");
+
+    init_gfx();
 
     while (1) {
         vsync();
