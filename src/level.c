556b6bb06385aed327b9ccd5b4414e0988cd45d7
Alex Precosky <precosky@gmail.com>
2026-02-15 20:05:30 -0800

    The player moves along the play grid now

--- src/level.c
+++ src/level.c
@@ -7,6 +7,7 @@
 #include "sprite_idx.h"
 #include <gb/gb.h>
 #include <gbdk/console.h>
+#include <stdbool.h>
 #include <stdint.h>
 #include <stdio.h>
 
@@ -15,6 +16,19 @@ const uint8_t SPRITE_NUM_PLAYER_BOT = 0;
 static entity_t s_entity_player_bot;
 
 static uint8_t s_player_bot_tile_seq[] = { ALX_BOT_THROBBER_FRAME_0, ALX_BOT_THROBBER_FRAME_1 };
+
+static const uint8_t GRID_TO_SCREEN_OFFSET_X = 21;
+static const uint8_t GRID_TO_SCREEN_OFFSET_Y = 28;
+static const uint8_t GRID_CELL_WIDTH = 8;
+static const uint8_t GRID_CELL_HEIGHT = 8;
+
+typedef struct {
+    uint8_t pos_x;
+    uint8_t pos_y;
+} position_t;
+
+static position_t grid_to_xy(position_t grid_position);
+static bool is_entity_at_vertex(const entity_t *entity);
 
 void init_gfx(void)
 {
@@ -29,7 +43,14 @@ void init_gfx(void)
     set_sprite_data(0, SPRITE_TILE_COUNT, sprites);
     entity_init(&s_entity_player_bot, SPRITE_NUM_PLAYER_BOT);
     entity_set_tile_sequence(&s_entity_player_bot, s_player_bot_tile_seq, 2);
-    entity_set_pos(&s_entity_player_bot, 20, 20);
+
+    position_t player_start_grid = {
+        .pos_x = 0,
+        .pos_y = 0,
+    };
+    position_t player_start_screen = grid_to_xy(player_start_grid);
+
+    entity_set_pos(&s_entity_player_bot, player_start_screen.pos_x, player_start_screen.pos_y);
 
     LCDC_REG |= LCDCF_OBJON; /* Enable the drawing of sprites */
     DISPLAY_ON;
@@ -46,19 +67,26 @@ static void update_inputs(const joypads_t *joypads)
         entity_set_input_dir_bitfield(&s_entity_player_bot, INPUT_DIR_UP);
     } else if (joypads->joy0 & J_DOWN) {
         entity_set_input_dir_bitfield(&s_entity_player_bot, INPUT_DIR_DOWN);
+    } else {
+        entity_set_input_dir_bitfield(&s_entity_player_bot, 0);
     }
 }
 
 static void update_velocities()
 {
-    if(s_entity_player_bot.input_dir_bitfield & INPUT_DIR_LEFT) {
-        s_entity_player_bot.velocity = VELOCITY_LEFT;
-    } else if (s_entity_player_bot.input_dir_bitfield & INPUT_DIR_RIGHT) {
-        s_entity_player_bot.velocity = VELOCITY_RIGHT;
-    } else if (s_entity_player_bot.input_dir_bitfield & INPUT_DIR_UP) {
-        s_entity_player_bot.velocity = VELOCITY_UP;
-    } else if (s_entity_player_bot.input_dir_bitfield & INPUT_DIR_DOWN) {
-        s_entity_player_bot.velocity = VELOCITY_DOWN;
+    if (is_entity_at_vertex(&s_entity_player_bot)) {
+        if (s_entity_player_bot.input_dir_bitfield & INPUT_DIR_LEFT) {
+            s_entity_player_bot.velocity = VELOCITY_LEFT;
+        } else if (s_entity_player_bot.input_dir_bitfield & INPUT_DIR_RIGHT) {
+            s_entity_player_bot.velocity = VELOCITY_RIGHT;
+        } else if (s_entity_player_bot.input_dir_bitfield & INPUT_DIR_UP) {
+            s_entity_player_bot.velocity = VELOCITY_UP;
+        } else if (s_entity_player_bot.input_dir_bitfield & INPUT_DIR_DOWN) {
+            s_entity_player_bot.velocity = VELOCITY_DOWN;
+        } else {
+            s_entity_player_bot.velocity = VELOCITY_STOP;
+        }
+
     }
 }
 
@@ -83,6 +111,32 @@ static void update_positions()
 static void move_entities()
 {
     move_sprite(SPRITE_NUM_PLAYER_BOT, s_entity_player_bot.pos_x, s_entity_player_bot.pos_y);
+}
+
+/**
+ * @brief Check if the entity is on top of a grid vertex
+ * @param entity - The entity to inspect the position of
+ * @return True if the entity is on top of a grid vertex
+ */
+static bool is_entity_at_vertex(const entity_t *entity) {
+    if (((entity->pos_x - GRID_TO_SCREEN_OFFSET_X) % GRID_CELL_WIDTH == 0) &&
+        ((entity->pos_y - GRID_TO_SCREEN_OFFSET_Y) % GRID_CELL_HEIGHT == 0)) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+/**
+ * @param grid_position - A position on the game grid
+ * @return The xy screen position of the game grid position
+ */
+static position_t grid_to_xy(position_t grid_position)
+{
+    position_t screen_position;
+    screen_position.pos_x = GRID_TO_SCREEN_OFFSET_X + grid_position.pos_x * GRID_CELL_WIDTH;
+    screen_position.pos_y = GRID_TO_SCREEN_OFFSET_Y + grid_position.pos_y * GRID_CELL_HEIGHT;
+    return screen_position;
 }
 
 void run_level(enum bot selected_bot, const joypads_t *joypads)
