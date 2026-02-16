651f007d7f97c5b550799986b53838ea9cbfa0c3
Alex Precosky <precosky@gmail.com>
2026-02-15 16:45:14 -0800

    Mute the music channels when leaving the title screen
    It would leave the music channel buzzing when the level starts...

--- src/title_screen.c
+++ src/title_screen.c
@@ -95,6 +95,8 @@ static void stop_sound()
     {
         remove_VBL(hUGE_dosound);
         remove_VBL(CBTFX_update);
+        hUGE_mute_channel(HT_CH2, 1);
+        hUGE_mute_channel(HT_CH4, 1);
     }
 }
 
