7a4db8d6c6bcb6446c79ede409bcc0bd9d40271b
Alex Precosky <precosky@gmail.com>
2026-03-29 21:45:09 -0700

    Added sprite_idx.c to source control - had forgotton to

---
+++ src/sprite_idx.c
@@ -0,0 +1,6 @@
+// Sprite sequences
+
+#include "sprite_idx.h"
+
+uint8_t alx_bot_tile_seq[2] = { ALX_BOT_THROBBER_FRAME_0, ALX_BOT_THROBBER_FRAME_1 };
+uint8_t srna_bot_tile_seq[2] = { SRNA_BOT_THROBBER_FRAME_0, SRNA_BOT_THROBBER_FRAME_1 };
